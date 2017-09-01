30cc25b03f9c6f5bca0b30daf1990e9a41950822
David Hunt <bitbucket.davidhunt@holodyn.com>
2017-09-01 14:53:06 -0400

    Added contact detection/linking

--- installation/modules/addons/wbticketclient/hooks.php
+++ installation/modules/addons/wbticketclient/hooks.php
@@ -25,36 +25,103 @@ defined("WHMCS") or die("wbTicketClient Error: Invalid File Access");
   use WHMCS\Session;
   use WHMCS\Ticket\Watchers;
   add_hook('AdminAreaViewTicketPage', 1, function($vars) {
+
+    /**
+     * Stage
+     */
     global $_ADMINLANG;
-    $html = array('<div class="addon_wbticketclient" style="position:relative;float:right;margin-top:-1rem;z-index:1;">');
     $adminId   = Session::get('adminid');
     $ticket_id = (int)@$_REQUEST['id'];
+
+    /**
+     * Confirm we're valid
+     */
     if ($adminId && $ticket_id) {
+
+      /**
+       * Load Database
+       */
       if( file_exists(ROOTDIR.'/modules/addons/wbDatabase.php') )
         require_once(ROOTDIR.'/modules/addons/wbDatabase.php');
       $dbh = wbDatabase::getInstance();
+
+      /**
+       * HTML Container
+       * Inline styles - lazy, small
+       */
+      $adminTemplate = $dbh->runQuery("SELECT template FROM tbladmins WHERE id = " . $adminId)->getValue();
+      $html = array(
+        ($adminTemplate == 'v4'
+          ? '<style>.addon_wbticketclient{position:relative;float:right;margin-right:.5rem;z-index:1;}</style>'
+          : '<style>.addon_wbticketclient{position:relative;float:right;margin-top:-1rem;z-index:1;}</style>'
+          ),
+        '<div class="addon_wbticketclient">'
+        );
+
+      /**
+       * Load Ticket
+       */
       if ($ticket = $dbh->runQuery("SELECT * FROM tbltickets WHERE id = " . $ticket_id)->getObject()) {
+
+        /**
+         * Import the Language
+         */
         $adminLang = $dbh->runQuery("SELECT language FROM tbladmins WHERE id = " . $adminId)->getValue();
         if ($adminLang && file_exists(__DIR__ . '/lang/'.$adminLang.'.php'))
           include __DIR__ . '/lang/'.$adminLang.'.php';
         else
           include __DIR__ . '/lang/english.php';
+
+        /**
+         * Are we orphaned?
+         */
         if (!$ticket->userid) {
-          if ($user_id = (int)@$_REQUEST['assign_to_client']) {
-            $dbh->runUpdate('tbltickets', array(
-              'userid' => $user_id
-              ), array(
-              "id = " . $ticket_id
-              ));
-            $dbh->runUpdate('tblticketreplies', array(
-              'userid' => $user_id
-              ), array(
-              "tid = " . $ticket_id,
-              "userid = 0",
-              "email = '" . $dbh->getEscaped($ticket->email) ."'"
-              ));
+
+          /**
+           * Was Client/Contact Assignment requested
+           * Update ticket, ticketreplies with userid and cc (contact email)
+           */
+          if (
+            $user_id = (int)@$_REQUEST['assign_to_client']
+            || $contact_id = (int)@$_REQUEST['assign_to_contact']
+            ) {
+            $cc_email - null;
+            if ($contact_id) {
+              if ($contact = $dbh->runQuery("SELECT * FROM tblcontacts WHERE id = ". (int)$contact_id)->getObject()){
+                $user_id = $contact->userid;
+                $cc_email = $contact->email;
+              }
+            }
+            if ($user_id) {
+              $user = $dbh->runQuery("
+                SELECT *
+                FROM tblclients
+                WHERE id = ". (int)$user_id)->getObject();
+              if ($user) {
+                $dbh->runQuery("
+                  UPDATE tbltickets
+                  SET userid = ". (int)$user_id ."
+                    ". ($cc_email ? ", cc = IF(length(cc) > 0, IF(find_in_set('". $dbh->getEscaped($cc_email) ."', cast(cc as char)) > 0,cc,CONCAT(cc,',','". $dbh->getEscaped($cc_email) ."')), '". $dbh->getEscaped($cc_email) ."')" : '') ."
+                  WHERE id = ". (int)$ticket_id ."
+                  ");
+                $dbh->runQuery("
+                  UPDATE tblticketreplies
+                  SET userid = ". (int)$user_id ."
+                    ". ($cc_email ? ", cc = IF(length(cc) > 0, IF(find_in_set('". $dbh->getEscaped($cc_email) ."', cast(cc as char)) > 0,cc,CONCAT(cc,',','". $dbh->getEscaped($cc_email) ."')), '". $dbh->getEscaped($cc_email) ."')" : '') ."
+                  WHERE tid = ". (int)$ticket_id ."
+                    AND userid = 0
+                    AND email = '" . $dbh->getEscaped($ticket->email) ."'
+                  ");
+              }
+            }
             header('Location: supporttickets.php?action=view&id='. $ticket_id);
           }
+
+          /**
+           * Was a Creation requested
+           * Create new user (directly since we don't have all required information)
+           * Update ticket, ticketreplies with the userid
+           */
           elseif (@$_REQUEST['create_client']) {
             $name = explode(' ', $ticket->name);
             if (count($name) > 1) {
@@ -100,16 +167,44 @@ defined("WHMCS") or die("wbTicketClient Error: Invalid File Access");
             }
             header('Location: supporttickets.php?action=view&id='. $ticket_id);
           }
+
+          /**
+           * Make Recommendations
+           */
           else {
+
+            /**
+             * We found a client with the same email
+             */
             if ($user = $dbh->runQuery("SELECT * FROM tblclients WHERE email = '". $dbh->getEscaped($ticket->email) . "'")->getObject()) {
               $html[] = '<a href="supporttickets.php?action=view&id='. $ticket_id .'&assign_to_client='. $user->id .'" class="btn btn-sm btn-primary">'. sprintf($_ADMINLANG['wbticketclient']['assign_to_client'], $user->id, trim($user->firstname.' '.$user->lastname)) .'</a>';
             }
+
+            /**
+             * We found a contact with the same email
+             */
+            else if ($contact = $dbh->runQuery("SELECT * FROM tblcontacts WHERE email = '". $dbh->getEscaped($ticket->email) . "'")->getObject()) {
+              $html[] = '<a href="supporttickets.php?action=view&id='. $ticket_id .'&assign_to_contact='. $contact->id .'" class="btn btn-sm btn-primary">'. sprintf($_ADMINLANG['wbticketclient']['assign_to_contact'], $contact->id, trim($contact->firstname.' '.$contact->lastname)) .'</a>';
+            }
+
+            /**
+             * Offer to create a new client
+             */
             else {
               $html[] = '<a href="supporttickets.php?action=view&id='. $ticket_id .'&create_client=1" class="btn btn-sm btn-primary">'. $_ADMINLANG['wbticketclient']['create_client'] .'</a>';
             }
+
           }
         }
+
+        /**
+         * Else we're alerady linked
+         */
         elseif ($user = $dbh->runQuery("SELECT * FROM tblclients WHERE id = " . $ticket->userid)->getObject()) {
+
+          /**
+           * Request to unlink client from ticket
+           */
           if (@$_REQUEST['unlink_client']) {
             $ticket_ids = $dbh->runQuery("
               SELECT id
@@ -134,10 +229,25 @@ defined("WHMCS") or die("wbTicketClient Error: Invalid File Access");
             }
             header('Location: supporttickets.php?action=view&id='. $ticket_id);
           }
+
+          /**
+           * Offer to unlink from client
+           */
           $html[] = '<a href="supporttickets.php?action=view&id='. $ticket_id .'&unlink_client=1" class="btn btn-sm btn-warning">'. $_ADMINLANG['wbticketclient']['unlink_client'] .'</a>';
+
         }
       }
+
+      /**
+       * Closeout the container
+       */
+      $html[] = '</div>';
+
+      /**
+       * Return outpu
+       */
+      return implode("\n", $html);
+
     }
-    $html[] = '</div>';
-    return implode("\n", $html);
+
   });
--- installation/modules/addons/wbticketclient/lang/english.php
+++ installation/modules/addons/wbticketclient/lang/english.php
@@ -11,3 +11,4 @@ defined("WHMCS") or die("wbTicketClient Error: Invalid File Access");
 $_ADMINLANG['wbticketclient']['unlink_client'] = '<i class="fa fa-user"></i> Unlink Client';
 $_ADMINLANG['wbticketclient']['create_client'] = '<i class="fa fa-user"></i> Create Client';
 $_ADMINLANG['wbticketclient']['assign_to_client'] = '<i class="fa fa-user"></i> Assign to %2$s, #%1$s';
+$_ADMINLANG['wbticketclient']['assign_to_contact'] = '<i class="fa fa-user"></i> Assign to %2$s, #%1$s';
